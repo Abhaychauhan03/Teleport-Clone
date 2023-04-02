f98bfaec7fd6dea1c0f45a9796dbddc2dbb93a8f
Abhay Singh Chauhan <chauhanabhay027@gmail.com>
2023-04-02 11:20:24 +0530

    added different css

--- components/DropCard.js
+++ components/DropCard.js
@@ -40,43 +40,55 @@ function DropCard() {
 
       {/* section */}
 
-      <div className="grid grid-rows-2 grid-cols-[0.5fr_1fr_1fr_1fr_1fr_1fr_1fr_0.5fr]">
-        <div className="col-span-6 col-start-2 row-span-1 flex space-x-4">
-          <div className="flex items-center">
-            <DocumentFilledIcon size="medium" />
+      <div className="relative grid grid-rows-1 md:grid-rows-2 grid-cols-[0.5fr_1fr_1fr_1fr_1fr_1fr_1fr_0.5fr]">
+        <div className="hidden md:flex col-span-1 md:col-span-6 row-span-6 md:col-start-2 md:row-span-1 flex-col md:flex-row space justify-around">
+          <div className="flex md:flex-col text-center items-center justify-center">
+            <div className="w-8 h-8 bg-card-gray text-center z-10 ">
+              <DocumentFilledIcon size="medium" />
+            </div>
+            <h3 className="font-normal text-sm text-[#525F75]">
+              Form completed
+            </h3>
           </div>
-          <div className="flex-grow h-full flex items-center">
-            <div className="h-[2px] flex-grow bg-line-color"></div>
+          <div className="flex md:flex-col text-center items-center justify-center">
+            <div className="w-8 h-8 bg-card-gray z-10 text-center">
+              <PortfolioIcon size="medium" />
+            </div>
+            <h3 className="font-normal text-sm text-[#525F75]">Processing</h3>
           </div>
-          <div className="flex items-center">
-            <PortfolioIcon size="medium" />
+          <div className="flex md:flex-col text-center items-center justify-center">
+            <div className="w-8 h-8 bg-card-gray z-10 text-center">
+              <MediaServicesSpreadsheetIcon size="medium" />
+            </div>
+            <h3 className="font-normal text-sm text-[#525F75]">Processed</h3>
           </div>
-          <div className="flex-grow h-full flex items-center">
-            <div className="h-[2px] flex-grow bg-line-color"></div>
+          <div className="bg-line-color h-[2px] w-[70%] absolute top-[20%]"></div>
+          <div className="flex md:flex-col text-center items-center justify-center">
+            <div className="w-8 h-8 bg-card-gray z-10 text-center">
+              <CalendarFilledIcon size="medium" />
+            </div>
+            <h3 className="font-normal text-sm text-[#525F75]">
+              Appointment booking
+            </h3>
           </div>
-          <div className="flex items-center">
-            <MediaServicesSpreadsheetIcon size="medium" />
+          <div className="flex md:flex-col text-center items-center justify-center">
+            <div className="w-8 h-8 bg-card-gray z-10 text-center">
+              <OfficeBuildingFilledIcon size="medium" />
+            </div>
+            <h3 className="font-normal text-sm text-[#525F75]">
+              At embassy/ VFS
+            </h3>
           </div>
-          <div className="flex-grow h-full flex items-center">
-            <div className="h-[2px] flex-grow bg-line-color"></div>
-          </div>
-          <div className="flex items-center">
-            <CalendarFilledIcon size="medium" />
-          </div>
-          <div className="flex-grow h-full flex items-center">
-            <div className="h-[2px] flex-grow bg-line-color"></div>
-          </div>
-          <div className="flex items-center">
-            <OfficeBuildingFilledIcon size="medium" />
-          </div>
-          <div className="flex-grow h-full flex items-center">
-            <div className="h-[2px] flex-grow bg-line-color"></div>
-          </div>
-          <div className="flex items-center">
-            <QuestionCircleIcon size="medium" />
+          <div className="flex md:flex-col text-center items-center justify-center">
+            <div className="w-8 h-8 bg-card-gray z-10 text-center">
+              <QuestionCircleIcon size="medium" />
+            </div>
+            <h3 className="font-normal text-sm text-[#525F75]">
+              Approval/Rejection
+            </h3>
           </div>
         </div>
-        <div className=" row-start-2 col-span-8 p-4">
+        <div className="col-start-1 col-span-8 md:col-start-auto md:row-start-2 md:col-span-8 p-4">
           <div className="bg-note-yellow p-4 rounded-sm">
             <div className="pb-3 px-4 border-b-2 border-line-yellow">
               <h3 className="text-sm font-medium">
@@ -98,7 +110,7 @@ function DropCard() {
 
       {/* section */}
 
-      <div className="hidden md:flex items-center justify-end px-5 space-x-1">
+      <div className="hidden md:flex items-center justify-end px-5 space-x-1 mb-4">
         <Toggle id="toggle-default" />
         <label htmlFor="toggle-default" className="text-sm font-medium">
           Send status updates on Whatsapp (+91 8800726381)
@@ -107,7 +119,7 @@ function DropCard() {
 
       {/* section */}
 
-      <div className="p-4 md:px-10 space-y-3">
+      <div className="p-4 md:px-10 space-y-3 border-t-2">
         <div className="flex justify-between">
           <UserCircleIcon className="h-6" />
           <h3>Amit Patel</h3>
